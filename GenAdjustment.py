91802ffa51c17fe0368ecf74f8d39b1a2db74bd0
Eduardo Answer <eduardonzinga111@hotmail.com>
2018-08-12 21:43:56 -0300

    Comecando o projeto

---
+++ GenAdjustment.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 21:16:38 2018
+
+@author: Eduardo Answer
+"""
+
+"""
+This programm is an implementation of Genetic Algorithm applied to Geodetic
+Networks Adjustment
+"""
+
+import numpy as np
+import matplotlib as plt
+
