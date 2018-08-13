06c37079ad42a86bdd63e8439ab9da3a605cf923
Eduardo Answer <eduardonzinga111@hotmail.com>
2018-08-13 20:15:40 -0300

    Carregamento das matrizes

--- GenAdjustment.py
+++ GenAdjustment.py
@@ -10,6 +10,29 @@ This programm is an implementation of Genetic Algorithm applied to Geodetic
 Networks Adjustment
 """
 
-import numpy as np
 import matplotlib as plt
+import math
 
+try:
+    import numpy as np
+except ImportError:
+    print("Numpy is not installed")
+from numpy import random
+
+A = np.loadtxt("A.txt", dtype = 'f')
+LB = np.loadtxt("LB.txt", dtype  = 'f')
+LO = np.loadtxt("LO.txt", dtype = 'f')
+MVC = np.loadtxt("MVC.txt", dtype = 'f')
+
+class parameter:
+    def __init__(self, A, LB, LO, MVC):
+        self.A = A
+        self.LB = LB
+        self.LO = LO
+        self.MVC = MVC
+        
+    def X_parameters(self):       
+        print(self.A)
+        
+matrices = parameter(A, LB, LO, MVC)
+matrices.X_parameters()
